f93a50f69e8fd51dc410f37f04a5e78c39bf973f
WarTeamX <github@warteamx.com>
2023-07-31 13:03:20 +0200

    🚀 Deploy Build

---
+++ build/precache-manifest.d4d76962d54156999b92dbb7fd060176.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "27ae55c79d9ba96b5bb4",
+    "url": "/static/css/main.38b39436.chunk.css"
+  },
+  {
+    "revision": "27ae55c79d9ba96b5bb4",
+    "url": "/static/js/main.9e8b3965.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4e0514220be7ea68e8f5",
+    "url": "/static/css/2.b0e3634d.chunk.css"
+  },
+  {
+    "revision": "4e0514220be7ea68e8f5",
+    "url": "/static/js/2.a755c138.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a6c7da004b56c708733b6b5f0c149652",
+    "url": "/static/media/hero.a6c7da00.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "33d0d665a75d17dd3b6674419eaaed15",
+    "url": "/static/media/odigeo-logo.33d0d665.jpg"
+  },
+  {
+    "revision": "a0673294787a1304d352a49555fff6e3",
+    "url": "/static/media/aguafy-logo.a0673294.png"
+  },
+  {
+    "revision": "801336abe70d7cdab6822220c567edff",
+    "url": "/static/media/bicos-logo.801336ab.png"
+  },
+  {
+    "revision": "a3a399c33486fdb79aacd246c36848a5",
+    "url": "/static/media/Veepee.a3a399c3.png"
+  },
+  {
+    "revision": "16274434b04a0adff9ddbea057690752",
+    "url": "/static/media/mytaxi-logo.16274434.png"
+  },
+  {
+    "revision": "8c999e99a9c2aa24794033fb2cf585e6",
+    "url": "/static/media/rakuten-logo.8c999e99.png"
+  },
+  {
+    "revision": "f98b52b83247b27017fdc1b625ad965a",
+    "url": "/static/media/Tinkar.f98b52b8.gif"
+  },
+  {
+    "revision": "21375f0a96247d24ffd71293d06c892e",
+    "url": "/static/media/rakuten.21375f0a.gif"
+  },
+  {
+    "revision": "56fdf6cff9d672b02f2273a5e762d1dc",
+    "url": "/static/media/edreams.56fdf6cf.gif"
+  },
+  {
+    "revision": "fb541d9c64a19a615dacc9b5f8da40ec",
+    "url": "/static/media/opodo.fb541d9c.gif"
+  },
+  {
+    "revision": "dcbe16d0daa0e95d70f4c750a6eb7671",
+    "url": "/static/media/datepan.dcbe16d0.gif"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "914b3e26b254e0c507c4700248d76a3b",
+    "url": "/index.html"
+  }
+];
